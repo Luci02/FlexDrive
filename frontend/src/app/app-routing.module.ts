c3e107e798c6f7e134d4c840a44e6f07aa68e8da
Luci02 <luciparlato50@gmail.com>
2023-08-31 12:28:50 +0200

    creato componente per la pagina home

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepagePage } from './pages/homepage/homepage.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomepagePage,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/pages/homepage/homepage.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './homepage.page.html',
+  styleUrls: ['./homepage.page.scss']
+})
+export class HomepagePage {
+
+}
